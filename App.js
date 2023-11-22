3340ca67fdf6d8a533edeb576ed534aea45a7934
ainosorsa <t1soai00@students.oamk.fi>
2023-11-22 20:49:06 +0200

    alkunäytön kokeilua

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import HomeScreen from './HomeScreen';
-import SearchPetScreen from './SearchPetScreen';
+import HomeScreen from './Screens/HomeScreen';
+//import SearchPetScreen from './Screens/SearchPetScreen';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -4,12 +4,12 @@ import {AntDesign} from '@expo/vector-icons';
 //import React, { useEffect, useState } from 'react';
 
 export default function HomeScreen({navigation}) {
-    const [message, setMessage] = useState('Tänne tulee etusivun esittely');
+    const [message, setMessage] = useState('PetRescue');
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerStyle: {
-                backgroundColor: 'f0f0f0'
+                backgroundColor: 'lightgreen'
             },
             headerRight: () => (
 
@@ -25,8 +25,10 @@ export default function HomeScreen({navigation}) {
 
     return (
         <View style={styles.container}>
-            <Text>Home screen {message}</Text>
-            <TextInput onChangeText={text => setMessage(text)} value={message} placeholder="Type Message here"/>
+            <Text style={styles.header}>Tervetuloa {message} sovellukseen!</Text>
+            <Text style={styles.infoText}>Etsitkö uskollista ystävää? Ystävää jonka kanssa jakaa elämän ilot ja surut? Haluaisitko mahdollisesti antaa Lemmikille uuden turvallisen kodin?</Text>
+            <Text style={styles.infoText}>Jos vastasit kyllä, olet oikeassa paikassa ja ehkäpä jopa askeleen lähempänä uutta ystävääsi!</Text>
+            <Text style={styles.infoText}>Olemme PetRescue ja haluamme toimia turvallisena välikätenä lemmikeiden adoptoimiseen.</Text>
         </View>
     )
 }
@@ -34,9 +36,25 @@ export default function HomeScreen({navigation}) {
 const styles = StyleSheet.create({
     container: {
         padding: 10,
+        
     },
     navButton: {
         marginRight: 5,
         padding: 4,
+    },
+    header : {
+        padding: 10,
+        width: 500,
+        fontWeight: '400',
+        fontStyle: 'italic',
+        fontSize: 30,
+        center: 3,  
+    },
+    infoText : {
+        padding: 15,
+        center: 1,
+        fontSize: 15
     }
+    
+    
 });
